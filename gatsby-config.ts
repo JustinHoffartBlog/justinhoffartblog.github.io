8bb7b16c6a9737aa4051c06e1d0e2ce1fc8f4466
Ali Yıldızöz <aliyildizoz909@gmail.com>
2024-05-12 00:46:25 +0300

    intial website

---
+++ gatsby-config.ts
@@ -0,0 +1,173 @@
+import type { GatsbyConfig, PluginRef } from "gatsby"
+import "dotenv/config"
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
+    siteTitle: `justin hoffart blog`,
+    siteTitleAlt: `justin hoffart blog`,
+    siteHeadline: `justin hoffart blog`,
+    siteUrl: `https://justinhoffartblog.github.io`,
+    siteDescription: `Explore our blog for insights on Angular, React, PostgreSQL, Docker, Azure, Entity Framework, ASP.NET Core, C#, and more. Stay ahead with expert tips and trends in programming languages and tech tools for software development!`,
+    siteImage: `/banner.jpg`,
+    siteLanguage: `en`,
+    author: `@justinhoffartblog`,
+  },
+  trailingSlash: `always`,
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      options: {
+        navigation: [
+          {
+            title: `blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `about`,
+            slug: `/about`,
+          }
+        ],
+        externalLinks: [
+          {
+            name: `linkedin`,
+            url: `https://www.linkedin.com/in/ali-yildizoz/`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/`,
+      },
+    },   
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `justin hoffart blog`,
+        short_name: `justinhoffartblog`,
+        description: `justinhoffartblog`,
+        start_url: `/`,
+        background_color: `#fff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title: siteTitle
+                description: siteDescription
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({
+              query: { site, allPost },
+            }: {
+              query: { allPost: IAllPost; site: { siteMetadata: ISiteMetadata } }
+            }) =>
+              allPost.nodes.map((post) => {
+                const url = site.siteMetadata.siteUrl + post.slug
+                const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`
+
+                return {
+                  title: post.title,
+                  date: post.date,
+                  excerpt: post.excerpt,
+                  url,
+                  guid: url,
+                  custom_elements: [{ "content:encoded": content }],
+                }
+              }),
+            query: `{
+  allPost(sort: {date: DESC}) {
+    nodes {
+      title
+      date(formatString: "MMMM D, YYYY")
+      excerpt
+      slug
+    }
+  }
+}`,
+            output: `rss.xml`,
+            title: `justin hoffart blog8`,
+          },
+        ],
+      },
+    },
+    // You can remove this plugin if you don't need it
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-statoscope`,
+      options: {
+        saveReportTo: `${__dirname}/public/.statoscope/_bundle.html`,
+        saveStatsTo: `${__dirname}/public/.statoscope/_stats.json`,
+        open: false,
+      },
+    },
+  ].filter(Boolean) as Array<PluginRef>
+}
+
+export default config
+
+interface IPostTag {
+  name: string
+  slug: string
+}
+
+interface IPost {
+  slug: string
+  title: string
+  defer: boolean
+  date: string
+  excerpt: string
+  contentFilePath: string
+  html: string
+  timeToRead: number
+  wordCount: number
+  tags: Array<IPostTag>
+  banner: any
+  description: string
+  canonicalUrl: string
+}
+
+interface IAllPost {
+  nodes: Array<IPost>
+}
+
+interface ISiteMetadata {
+  siteTitle: string
+  siteTitleAlt: string
+  siteHeadline: string
+  siteUrl: string
+  siteDescription: string
+  siteImage: string
+  author: string
+}
